437a980315208ddea2e6da14e16e7f2f7539e76d
Chandler <1941719093@qq.com>
2017-03-29 22:12:01 +0800

    success

--- dubbox-api/src/main/java/com/ysh/dubbox/api/UserService.java
+++ dubbox-api/src/main/java/com/ysh/dubbox/api/UserService.java
@@ -2,12 +2,25 @@ package com.ysh.dubbox.api;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import com.alibaba.dubbo.rpc.protocol.rest.support.ContentType;
 import com.ysh.dubbox.api.model.User;
 
-
+@Path("u")
+@Consumes({MediaType.APPLICATION_JSON, MediaType.TEXT_XML})
+@Produces({ContentType.APPLICATION_JSON_UTF_8, ContentType.TEXT_XML_UTF_8})
 public interface UserService {
-
+	
+	@GET
+    @Path("")
 	List<User> findAll();
-
+	
+	@POST
+    @Path("create")
 	String create(User user);
 }
